c2398a951d133d4c76a73eabfba11a0acb9319a9
GuHaijun <243297288@qq.com>
2018-04-13 16:09:58 +0800

    #fix 修改文件头部创建信息

--- NNNavigationBar/Utils/NSLayoutConstraint+NNVisualFormat.m
+++ NNNavigationBar/Utils/NSLayoutConstraint+NNVisualFormat.m
@@ -1,6 +1,6 @@
 //
 //  NSLayoutConstraint+NNVisualFormat.m
-//  NNNavigationBarDemo
+//  NNNavigationBar
 //
 //  Created by GuHaijun on 2018/4/13.
 //  Copyright © 2018年 GuHaijun. All rights reserved.
