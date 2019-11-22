80f1044c9c9a911c2290e158b6f0ebca83f7d679
HaiJun Gu <243297288@qq.com>
2019-11-23 01:08:32 +0800

    修改不规范变量命名

--- NNNavigationBar/NNNavigationBar/Core/NNTransitionClass.m
+++ NNNavigationBar/NNNavigationBar/Core/NNTransitionClass.m
@@ -18,39 +18,40 @@ mach_header_64
 #else
 mach_header
 #endif
-nn_pop_mach_header;
+nn_nav_mach_header;
 
 unsigned int nn_transitionClazzCount = 0;
 nn_transition_clazz_t *nn_transitionClazzes = nil;
 
-static pthread_mutex_t nn_pop_injectLock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t nn_nav_injectLock = PTHREAD_MUTEX_INITIALIZER;
 
 /// Loads protocol extensions info from image segment
 /// @param mhp A mach header appears at the very beginning of the object file
-void nn_pop_loadSection(const struct mach_header* mhp, intptr_t vmaddr_slide) {
+void nn_nav_loadSection(const struct mach_header* mhp, intptr_t vmaddr_slide) {
     
-    if (pthread_mutex_lock(&nn_pop_injectLock) != 0) {
+    if (pthread_mutex_lock(&nn_nav_injectLock) != 0) {
         fprintf(stderr, "ERROR: Could not synchronize on special protocol data\n");
     }
     
     unsigned long size = 0;
-    uintptr_t *sectionData = (uintptr_t*)getsectiondata((nn_pop_mach_header *)mhp, nn_segment_name, nn_section_name, &size);
+    uintptr_t *sectionData = (uintptr_t*)getsectiondata((nn_nav_mach_header *)mhp, nn_segment_name, nn_section_name, &size);
     if (size == 0) {
-        pthread_mutex_unlock(&nn_pop_injectLock);
+        pthread_mutex_unlock(&nn_nav_injectLock);
         return;
     }
     nn_transitionClazzes = (nn_transition_clazz_t *)malloc(size);
     if (nn_transitionClazzes == nil) {
-        pthread_mutex_unlock(&nn_pop_injectLock);
+        pthread_mutex_unlock(&nn_nav_injectLock);
         return;
     }
     memset(nn_transitionClazzes, 0, size);
     memcpy(nn_transitionClazzes, sectionData, size);
     nn_transitionClazzCount = (unsigned int)(size / sizeof(nn_transition_clazz_t));
     
-    pthread_mutex_unlock(&nn_pop_injectLock);
+    pthread_mutex_unlock(&nn_nav_injectLock);
 }
 
 __attribute__((constructor)) void nn_nav_initializer(void) {
-    _dyld_register_func_for_add_image(nn_pop_loadSection);
+    
+    _dyld_register_func_for_add_image(nn_nav_loadSection);
 }
